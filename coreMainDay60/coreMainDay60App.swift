0acb85d8abedd5a8eade98940f0470bc38343748
ganudoomer <ganuiscool@gmail.com>
2021-11-30 00:57:33 +0530

    Initial Commit

---
+++ coreMainDay60/coreMainDay60App.swift
@@ -0,0 +1,17 @@
+//
+//  coreMainDay60App.swift
+//  coreMainDay60
+//
+//  Created by Sree on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct coreMainDay60App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
